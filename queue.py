8b5e097fa1dfd025ba26dd200617c009139b137a
forrest liao <z9508010531@gmail.com>
2019-04-10 18:34:43 +0800

    2019_0410 program update

--- queue.py
+++ queue.py
@@ -1,27 +1,32 @@
 import numpy as np
 
 class ArrayQueue:
-
     queue = []
     def __init__(self, capacity):
         self.capacity = capacity
         self.front = 0
         self.rear = 0
+
     def is_empty(self):
-        if self.front == self.rear:
-            return True
-        else:
+        if self.queue:
             return False
+        else:
+            return True
+
     def is_full(self):
-        if (self.rear)%self.capacity==self.front:
+        if self.is_empty():
+           return False
+        elif self.rear == self.front:
             return True
         else:
             return False
+
     def double_capacity(self):
-        new_queue = self.queue[self.front:]+self.queue[:self.rear]
-        self.queue = new_queue
-        del new_queue
-        self.capacity *=2
+        self.queue = self.queue[self.front:] + self.queue[:self.rear]
+        self.rear = self.capacity
+        self.capacity*= 2
+        self.front = 0
+
     def print_queue(self):
         if self.is_empty():
             print("This queue is empty.")
@@ -29,16 +34,22 @@ class ArrayQueue:
             print("The queue is {}.".format(self.queue[self.front:self.rear]))
         else:
             print("The queue is {}.".format(self.queue[self.front:]+self.queue[:self.rear]))
+
     def pop(self):
         if self.is_empty():
             print("This queue is empty,you can't dequeue")
         else:
-            self.front=(self.front+1)%self.capacity
+            self.front= (self.front+1)%self.capacity
+
     def push(self, num):
         if self.is_full():
             self.double_capacity()
-        self.queue+= [num]
+        if self.rear < self.front:
+            self.queue[self.rear] = num
+        else:
+            self.queue+= [num]
         self.rear = (self.rear+1)%self.capacity
+
 if __name__ == "__main__":
     queue1 = ArrayQueue(5)
     queue1.push("A")
@@ -46,12 +57,23 @@ if __name__ == "__main__":
     queue1.push("C")
     queue1.push("D")
     queue1.push("E")
+    queue1.pop()
     queue1.push("F")
     queue1.pop()
-    queue1.pop()
     queue1.push("G")
+    queue1.push("H")
+    queue1.push("I")
     queue1.pop()
-
+    queue1.pop()
+    queue1.pop()
+    queue1.push("J")
+    queue1.push("K")
+    queue1.push("L")
+    queue1.push("M")
+    queue1.push("N")
+    queue1.push("O")
+    queue1.push("P")
+    print("c:{},f:{},r:{},q:{}".format(queue1.capacity, queue1.front, queue1.rear, queue1.queue))
     queue1.print_queue()
 
 
